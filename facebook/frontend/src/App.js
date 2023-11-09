118871c1cec59d7ac81163d5461b53cc216fb4c4
Omar <ohammad@gwu.edu>
2023-11-09 12:57:00 -0500

    Unstyled login page

--- facebook/frontend/src/App.js
+++ facebook/frontend/src/App.js
@@ -1,6 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LoginForm from './components/LoginForm/index';
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route path="/">
+        <LoginForm />
+      </Route>
+    </Switch>
   );
 }
 
