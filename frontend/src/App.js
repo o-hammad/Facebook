730f2a538fbb7765df0080d47bd3fe9da28b716f
Omar <ohammad@gwu.edu>
2023-12-14 16:38:28 -0500

    add Google Analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import LoginForm from './components/LoginForm/index';
-import Navigation from './components/Navigation';
 import UserProfile from './components/UserProfile';
 import NewsFeed from './components/NewsFeed';
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
+import RouteChangeTracker from './components/RouteChangeTracker';
+
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-XVWCGS3WX9";
+
+
+ReactGA.initialize(TRACKING_ID);
+
+ReactGA.event({
+  category: 'User',
+  action: 'Created an Account'
+});
+
+ReactGA.exception({
+  description: 'An error ocurred',
+  fatal: true
+});
 
 
 function App() {
@@ -20,14 +35,17 @@ function App() {
   
 
   return (
-    <Switch>
-      <AuthRoute exact path="/" component={LoginForm}></AuthRoute>
+    <>
+      <RouteChangeTracker />
+      <Switch>
+        <AuthRoute exact path="/" component={LoginForm}></AuthRoute>
 
-      <ProtectedRoute exact path="/users/:userId" component={UserProfile}></ProtectedRoute>
+        <ProtectedRoute exact path="/users/:userId" component={UserProfile}></ProtectedRoute>
 
-      <ProtectedRoute exact path="/newsfeed" component={NewsFeed}></ProtectedRoute>
-      
-    </Switch>
+        <ProtectedRoute exact path="/newsfeed" component={NewsFeed}></ProtectedRoute>
+        
+      </Switch>
+    </>
   )
 }
 
